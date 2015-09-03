384d0b2e241988b0b432bc2c47e897b5c4f1bfb6
dentych <dennis@tychsen.me>
2015-09-03 13:57:09 +0200

    Added AnimalSimulation

---
+++ Lab2/AnimalSimulation/AnimalSimulation/AnimalSimulation/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalSimulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Square s = new Square();
+            s.Eat(50);
+
+            Random rand = new Random();
+            rand.Next(2);
+        }
+    }
+}
