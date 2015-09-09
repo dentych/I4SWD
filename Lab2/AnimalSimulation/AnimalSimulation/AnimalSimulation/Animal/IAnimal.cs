ae2230494a3c74a5fe4c7fc3213ee4d2b976be58
dentych <dennis@tychsen.me>
2015-09-09 11:17:22 +0200

    AnimalSim stuff & add ReportGenerator

---
+++ Lab2/AnimalSimulation/AnimalSimulation/AnimalSimulation/Animal/IAnimal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalSimulation
+{
+    public interface IAnimal
+    {
+        string Name { get; }
+        int Weight { get; }
+        bool Alive { get; }
+        int Health { get; }
+
+        /* Methods */
+        int[] GetPosition();
+        
+        void NewDay();
+    }
+}
