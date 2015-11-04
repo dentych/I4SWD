7f1231dc857fab24dccce262d90ed116aec604b5
Dennis T <dennis@tychsen.me>
2015-11-04 11:44:32 +0100

    :flashlight: switch case now working :poop: :poop:

---
+++ FlashlightSwitchCase/FlashlightSwitchCase/Flashlight.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlashlightSwitchCase
+{
+    public class Flashlight
+    {
+        public enum FlashLightEvent { PowerBtnPressed }
+        enum FlashLightState { On, Off }
+
+        private FlashLightState _currentState;
+
+        public Flashlight()
+        {
+            _currentState = FlashLightState.Off;
+            Off();
+        }
+
+        public void HandleEvent(FlashLightEvent evt)
+        {
+            switch (_currentState)
+            {
+                case FlashLightState.On:
+                    _currentState = FlashLightState.Off;
+                    Off();
+                    break;
+
+                case FlashLightState.Off:
+                    _currentState = FlashLightState.On;
+                    On();
+                    break;
+            }
+        }
+
+        private void Off()
+        {
+            Console.WriteLine("I is off");
+        }
+
+        private void On()
+        {
+            Console.WriteLine("I is on");
+        }
+
+    }
+}
---
+++ FlashlightSwitchCase/FlashlightSwitchCase/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlashlightSwitchCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var light = new Flashlight();
+
+            for (int i = 0; i < 10000; i++)
+            {
+                light.HandleEvent(Flashlight.FlashLightEvent.PowerBtnPressed);
+            }
+        }
+    }
+}
